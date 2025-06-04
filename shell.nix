0a590436fb67f52df57126bce98ab34a522d1a0f
Finn Hodgkin <finnhodgkin@gmail.com>
2025-06-04 11:56:57 +0100

    Remove some LSP features

--- shell.nix
+++ shell.nix
@@ -1,22 +1,15 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-  easy-ps = import
-    (pkgs.fetchFromGitHub {
-      owner = "justinwoo";
-      repo = "easy-purescript-nix";
-      rev = "117fd96acb69d7d1727df95b6fde9d8715e031fc";
-      sha256 = "lcIRIOFCdIWEGyKyG/tB4KvxM9zoWuBRDxW+T+mvIb0=";
-    }) {
-    inherit pkgs;
-  };
-in
+
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    nodejs-18_x
+    nodejs_20
 
-    easy-ps.purs-0_15_14
-    easy-ps.spago
-    easy-ps.psa
+    # PureScript tools directly from nixpkgs instead of easy-purescript-nix
+    purescript
+    spago
 
+    # Additional useful tools
+    nodePackages.npm
+    git  # Required for spago to fetch dependencies
   ];
 }
